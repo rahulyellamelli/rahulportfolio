19adb87ab617b256ed3778d2e6f97d178be5567d
rahulyellamelli <rahulnashik143@gmail.com>
2021-07-23 23:10:23 +0530

    deployed

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,27 +1,27 @@
 export const SocialData = [
   {
     platform: "Instagram",
-    link: "www.instagram.com/rahul_rousey",
+    link: "https://instagram.com/rahul_rousey",
     icon: require("../../assets/icons/instagram.png").default,
   },
   {
     platform: "FaceBook",
-    link: "www.facebook.com/rahulyellamelli",
+    link: "https://facebook.com/rahulyellamelli",
     icon: require("../../assets/icons/facebook.png").default,
   },
   {
     platform: "Twitter",
-    link: "www.twitter.com/rahulyellamelli",
+    link: "https://twitter.com/rahulyellamelli",
     icon: require("../../assets/icons/twitter.png").default,
   },
   {
     platform: "LinkedIn",
-    link: "www.twitter.com/rahulyellamelli",
+    link: "https://www.linkedin.com/in/rahul-yellamelli-243538191",
     icon: require("../../assets/icons/linkedin.png").default,
   },
   {
     platform: "Git-Hub",
-    link: "www.github.com/rahulyellamelli",
+    link: "https://www.github.com/rahulyellamelli",
     icon: require("../../assets/icons/github.png").default,
   },
 ];
