3836b453406ed88546a60f8dc056d2122e2ad267
rahulyellamelli <rahulnashik143@gmail.com>
2021-07-25 13:10:59 +0530

    deployed

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,8 +5,8 @@ export const ProjectData = [
     about:
       "This project is based on creating a Giphy App which shows whatever Giphy searched in the search bar and Names List which stores the each and every name given in the nameslist",
     tags: ["ReactJS", "Css", "Html"],
-    demo: "https://www.youtube.com/watch?v=vYF_gLh2cz0",
-    github: "https://www.github.com/rahulyellamelli/Reactjs_project",
+    demo: "https://www.youtube.com/watch?v=vYF_gLh2cz0/index.html",
+    github: "https://www.github.com/rahulyellamelli/Reactjs_project/index.html",
     image:
       "https://www.wyzowl.com/wp-content/uploads/2019/07/GIPHY-Cam-1024x640.png",
   },
@@ -16,7 +16,7 @@ export const ProjectData = [
     about:
       "This project is based on creating a Food Order App in which we can search our favourite food and order it whenever we want",
     tags: ["ReactJS", "TailwindCss", "Html"],
-    demo: "https://www.youtube.com/watch?v=gOQ31Kc8H5E",
+    demo: "https://www.youtube.com/watch?v=gOQ31Kc8H5E/index.html",
     github: "www.github.com/rahulyellamelli/react-tailwind",
     image:
       "https://codecanyon.img.customer.envatousercontent.com/files/311559414/Inline+Preview.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=c3b6d68c30a81174457ddf4bec4a996d",
--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,27 +1,27 @@
 export const SocialData = [
   {
     platform: "Instagram",
-    link: "https://instagram.com/rahul_rousey",
+    link: "https://instagram.com/rahul_rousey/index.html",
     icon: require("../../assets/icons/instagram.png").default,
   },
   {
     platform: "FaceBook",
-    link: "https://facebook.com/rahulyellamelli",
+    link: "https://facebook.com/profile.php?id=100009666015155/index.html",
     icon: require("../../assets/icons/facebook.png").default,
   },
   {
     platform: "Twitter",
-    link: "https://twitter.com/rahulyellamelli",
+    link: "https://twitter.com/rahulyellamelli/index.html",
     icon: require("../../assets/icons/twitter.png").default,
   },
   {
     platform: "LinkedIn",
-    link: "https://www.linkedin.com/in/rahul-yellamelli-243538191",
+    link: "https://www.linkedin.com/in/rahul-yellamelli-243538191/index.html",
     icon: require("../../assets/icons/linkedin.png").default,
   },
   {
     platform: "Git-Hub",
-    link: "https://www.github.com/rahulyellamelli",
+    link: "https://www.github.com/rahulyellamelli/index.html",
     icon: require("../../assets/icons/github.png").default,
   },
 ];
